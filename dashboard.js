4f5cd006ca6148c2a26c08e000aa74b6294e05fa
UnitaryIron <emmanuellijo670@gmail.com>
2025-04-10 15:40:31 +0530

    Delete dashboard.js

--- dashboard.js
+++
@@ -1,8 +0,0 @@
-const name = localStorage.getItem("userName");
-document.getElementById("user-name").textContent = name ?? "User";
-
-// Logout function
-function logout() {
-  localStorage.clear();
-  window.location.href = "login.html"; // or your home page
-}
